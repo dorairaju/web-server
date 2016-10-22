ef801ddf80cd334735d61379b9a2e059cc67c080
Dorairaju <dorairaj512@gmail.com>
2016-10-22 17:42:22 -0500

    seperated middleware code form the server file

---
+++ middleware.js
@@ -0,0 +1,16 @@
+// Use module exports to expose middleware
+
+	var middleware = {
+	requireAuthentication: function (req, res, next) {
+		console.log('private route hit!');
+		next();
+	},
+	logger: function (req, res, next) {
+		// new Date().toString()
+		var date = new Date().toString();
+		console.log('Request: '+ date + req.method + ' ' + req.originalUrl);
+		next();
+	}
+	};
+
+module.exports = middleware;
