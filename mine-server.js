872ea10575e203534399be1b42b824841dd80606
Dorairaju <dorairaj512@gmail.com>
2016-10-22 17:23:51 -0500

    Initialize repo

---
+++ mine-server.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var app = express();
+var port = 3000;
+
+var middleware = {
+	requireAuthentication: function (req, res, next) {
+		console.log('private route hit!');
+		next();
+	},
+	logger: function (req, res, next) {
+		// new Date().toString()
+		var date = new Date().toString();
+		console.log('Request: '+ date + req.method + ' ' + req.originalUrl);
+		next();
+	}
+};
+
+app.use(middleware.logger);
+
+//app.use(middleware.requireAuthentication);
+
+app.get('/about', middleware.requireAuthentication, function (req, res) {
+	res.send('About us.');
+});
+
+app.use(express.static(__dirname + '/public'));
+
+
+
+//console.log(__dirname);
+app.listen(port, function () {
+	console.log('Express server started! at port no: '+port);
+});
